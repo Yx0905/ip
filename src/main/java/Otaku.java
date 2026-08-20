fc3e6918e2de5871304b40867507f29d49f6fec2
Yuxiang_Liu <liuyuxiang096@gmail.com>
2026-08-20 09:59:51 +0800

    Add Otaku greeting and exit flow

--- src/main/java/Otaku.java
+++ src/main/java/Otaku.java
@@ -1,10 +1,19 @@
+/** Starts Otaku and prints its greeting. */
 public class Otaku {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
         String banner = "  ___ _____  _    _  ___   _ \n"
                 + " / _ \\_   _|/ \\  | |/ / | | |\n"
                 + "| | | || | / _ \\ | ' /| | | |\n"
                 + "| |_| || |/ ___ \\| . \\| |_| |\n"
-                + " \\___/ |_/_/   \\_\\_|\\_\\\\___/ \n";
+                + " \\___/ |_/_/   \\_\\_|\\_\\\\___/";
+
+        System.out.println(divider);
         System.out.println(banner);
+        System.out.println("Hello! I'm Otaku.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
