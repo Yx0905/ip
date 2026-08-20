409950f23eb9f0c7c0e27bf5c15afbb60710a2b7
Yuxiang_Liu <liuyuxiang096@gmail.com>
2026-08-20 09:34:12 +0800

    Rename Duke chatbot to Otaku

---
+++ src/main/java/Otaku.java
@@ -0,0 +1,10 @@
+public class Otaku {
+    public static void main(String[] args) {
+        String banner = "  ___ _____  _    _  ___   _ \n"
+                + " / _ \\_   _|/ \\  | |/ / | | |\n"
+                + "| | | || | / _ \\ | ' /| | | |\n"
+                + "| |_| || |/ ___ \\| . \\| |_| |\n"
+                + " \\___/ |_/_/   \\_\\_|\\_\\\\___/ \n";
+        System.out.println(banner);
+    }
+}
